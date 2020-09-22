0f4c6cbae2d885a150f081a3fc629908c36bf7ac
Tetsuya-Yamada <tetsu.steel.iron.1222@gmail.com>
2020-09-22 19:08:15 +0900

    debug

--- analysistools/norm_fre_mp.py
+++ analysistools/norm_fre_mp.py
@@ -464,3 +464,7 @@ if __name__ == '__main__':
     elif method == 'time_bifurcation_rep':
         channel = arg[5]
         norm.time_bifurcation_rep(filename, channel)
+    elif method == 'time_bifurcation_all':
+        channel = arg[5]
+        magnif = arg[6]
+        norm.time_bifurcation_all(filename, channel, magnif)
